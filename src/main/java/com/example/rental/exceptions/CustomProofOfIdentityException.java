88cb3830d624f2f3603cb039397642a97d6de306
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-08-20 15:09:15 +0300

    feat: add exception

---
+++ src/main/java/com/example/rental/exceptions/CustomProofOfIdentityException.java
@@ -0,0 +1,7 @@
+package com.example.rental.exceptions;
+
+public class CustomProofOfIdentityException extends RuntimeException {
+  public CustomProofOfIdentityException(String massage) {
+    super(massage);
+  }
+}
