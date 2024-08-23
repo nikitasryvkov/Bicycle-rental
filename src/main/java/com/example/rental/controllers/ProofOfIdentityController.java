6dcf73743a464c764e44bb18c24851da0a8c3243
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-08-23 17:34:50 +0300

    feat: add ProofOfIdentityController

---
+++ src/main/java/com/example/rental/controllers/ProofOfIdentityController.java
@@ -0,0 +1,25 @@
+package com.example.rental.controllers;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rental.services.impl.ProofOfIdentityServiceImpl;
+
+@RestController
+@RequestMapping("/api/proof")
+public class ProofOfIdentityController {
+
+  @Autowired
+  private ProofOfIdentityServiceImpl proofOfIdentityServiceImpl;
+
+  @PostMapping("/verification")
+  public void verificationProofOfIdentity(@RequestParam(name = "clientId") int id, @RequestParam(name = "date") LocalDate localDate) {
+    proofOfIdentityServiceImpl.verificationProofOfIdentity(id, localDate);
+  }
+
+}
