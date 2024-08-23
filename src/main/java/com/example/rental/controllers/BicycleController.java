f35f352813a07bfe1e7e2f439f03ac860f9cf64a
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-08-23 17:33:27 +0300

    fix: add createBicycle

--- src/main/java/com/example/rental/controllers/BicycleController.java
+++ src/main/java/com/example/rental/controllers/BicycleController.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.rental.dtos.BicycleByAnthropometricDTO;
+import com.example.rental.dtos.BicycleDTO;
+import com.example.rental.domain.enums.BicycleStatus;
+import com.example.rental.domain.enums.BicycleType;
 import com.example.rental.dtos.BicycleByFilterDTO;
+import com.example.rental.dtos.BicycleDTO;
 import com.example.rental.services.impl.BicycleServiceImpl;
 
 @RestController
@@ -21,7 +25,7 @@ public class BicycleController {
   public BicycleServiceImpl bicycleServiceImpl;
 
   @GetMapping("/bicycleByAnth")
-  public List<BicycleByAnthropometricDTO> getBicycleByAnthropometric(@RequestParam(name = "id") int id) {
+  public List<BicycleDTO> getBicycleByAnthropometric(@RequestParam(name = "id") int id) {
     return bicycleServiceImpl.getBicycleByAnthropometricByClient(id);
   }
 
@@ -29,11 +33,27 @@ public class BicycleController {
   public List<BicycleByFilterDTO> getBicycleByFilter(
       @RequestParam(name = "manufacturer", required = false) String manufacturer,
       @RequestParam(name = "model", required = false) String model,
-      @RequestParam(name = "type", required = false) String type,
+      @RequestParam(name = "bicycleType", required = false) BicycleType bicycleType,
       @RequestParam(name = "brakesType", required = false) String brakesType,
       @RequestParam(name = "costPerDay", required = false) BigDecimal costPerDay) {
 
-    BicycleByFilterDTO bicycleByFilterDTO = new BicycleByFilterDTO(manufacturer, model, type, brakesType, costPerDay);
+    BicycleByFilterDTO bicycleByFilterDTO = new BicycleByFilterDTO(manufacturer, model, bicycleType, brakesType, costPerDay);
     return bicycleServiceImpl.getBicycleByFilter(bicycleByFilterDTO);
+  }
+
+  @PostMapping("/add")
+  public BicycleDTO createBicycle(
+    @RequestParam(name = "manufacturer") String manufacturer,
+    @RequestParam(name = "model") String model,
+    @RequestParam(name = "type") BicycleType bicycleType,
+    @RequestParam(name = "numberOfSpeeds") int numberOfSpeeds,
+    @RequestParam(name = "maxHeight") int maxHeight,
+    @RequestParam(name = "maxWeight") int maxWeight,
+    @RequestParam(name = "brakesType") String brakesType,
+    @RequestParam(name = "costPerDay") BigDecimal costPerDay,
+    @RequestParam(name = "bicycleStatus") BicycleStatus bicycleStatus) {
+
+    BicycleDTO bicycleDTO = new BicycleDTO(manufacturer, model, bicycleType, numberOfSpeeds, maxHeight, maxWeight, brakesType, costPerDay, bicycleStatus);
+    return bicycleServiceImpl.addBicycle(bicycleDTO);
   }
 }
