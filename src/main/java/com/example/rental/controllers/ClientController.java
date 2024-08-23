81d9479620b1fa1bfb1d9aa271e7c56379f92a40
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-08-23 17:34:08 +0300

    fix: change fields type

--- src/main/java/com/example/rental/controllers/ClientController.java
+++ src/main/java/com/example/rental/controllers/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.rental.domain.enums.GenderType;
 import com.example.rental.dtos.ClientDTO;
 import com.example.rental.dtos.ProofOfIdentityDTO;
 import com.example.rental.services.impl.DomainClientServiceImpl;
@@ -22,14 +23,14 @@ public class ClientController {
     @RequestParam(name = "firstName") String firstName,
     @RequestParam(name = "lastName") String lastName,
     @RequestParam(name = "middleName") String middleName,
-    @RequestParam(name = "gender") String gender,
+    @RequestParam(name = "gender") GenderType genderType,
     @RequestParam(name = "height") int height,
     @RequestParam(name = "weight") int weight,
     @RequestParam(name = "phoneNumber") String phoneNumber,
     @RequestParam(name = "documentType") String documentType,
     @RequestParam(name = "numberAndSeries") String numberAndSeries) {
 
-    ClientDTO clientDTO = new ClientDTO(firstName, lastName, middleName, gender, height, weight, phoneNumber);
+    ClientDTO clientDTO = new ClientDTO(firstName, lastName, middleName, genderType, height, weight, phoneNumber);
     ProofOfIdentityDTO proofOfIdentityDTO = new ProofOfIdentityDTO(documentType, numberAndSeries);
     return domainClientServiceImpl.addClient(clientDTO, proofOfIdentityDTO);
   }
